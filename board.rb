e9ef16bb2c1467fe94528791bd28f134f4ff0bab
Travis <tzito93@gmail.com>
2022-02-19 11:57:29 -0600

    initial board structure with methods for getting and settings board positions

--- board.rb
+++ board.rb
@@ -1,3 +1,19 @@
 class Board
+  attr_accessor :rows
+    
+  def initialize
+    @rows = Array.new(3) { Array.new(3) }
+  end
 
+  # use board[pos] where pos is [row, col] to easily access board positions
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+
+  # sets board[pos] to a given symbol
+  def []=(pos, symbol)
+    row, col = pos
+    @rows[row][col] = symbol
+  end  
 end
