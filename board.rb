7d17463951e21a8f42bcf6f1d62bf48d25b5a922
Travis <tzito93@gmail.com>
2022-02-21 18:46:23 -0600

    methods for checking both diagonals for matches and checking for a winner

--- board.rb
+++ board.rb
@@ -1,6 +1,6 @@
 class Board
   attr_accessor :rows
-    
+
   def initialize
     @rows = Array.new(3) { Array.new(3, '_') }
   end
@@ -16,7 +16,7 @@ class Board
     row, col = pos
     @rows[row][col] = symbol
   end
-  
+
   def render
     @rows.each do |row|
       row.each_with_index do |box, idx|
@@ -52,8 +52,32 @@ class Board
     false
   end
 
-  def check_diagonals
+  # taken from heraldry, meaning a diagonal going from top left to bottom right
+  def check_diagonal_dexter
+    positions = []
+    3.times do |i|
+      positions << self[[i, i]]
+    end
+    return true if positions.all? { |ele| ele == :X } || positions.all? { |ele| ele == :O }
 
+    false
   end
 
+  # taken from heraldry, meaning a diagonal going from bottom left to top right
+  def check_diagonal_sinister
+    positions = []
+    offset = @rows.length - 1
+    3.times do |i|
+      positions << self[[offset, i]]
+      offset -= 1
+    end
+    return true if positions.all? { |ele| ele == :X } || positions.all? { |ele| ele == :O }
+
+    false
+  end
+
+  def check_diagonals
+    check_diagonal_dexter || check_diagonal_sinister
+  end
+  # end of winner? helper methods
 end
