d4c93aa456dcebd234416ecf28ffebd7db0c4468
Travis <tzito93@gmail.com>
2022-04-11 16:22:25 -0500

    tests for column checking

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -127,6 +127,42 @@ describe Board do
   end
 
   describe '#check_cols' do
+    context 'when all columns are empty' do
+      it 'returns false' do
+        result = board.check_cols
+        expect(result).to be false
+      end
+    end
+
+    context 'when a column is full but not a three of a kind match' do
+      before do
+        board.instance_variable_set(:@rows, [
+            [:X, '_', '_'],
+            [:X, '_', '_'],
+            [:O, '_', '_']
+        ])
+      end
+
+      it 'returns false' do
+        result = board.check_cols
+        expect(result).to be false
+      end
+    end
+
+    context "when a column contains three X's" do
+      before do
+        board.instance_variable_set(:@rows, [
+            ['_', '_', :X],
+            ['_', '_', :X],
+            ['_', '_', :X]
+        ])
+      end
+
+      it 'returns true' do
+        result = board.check_cols
+        expect(result).to be true
+      end
+    end
   end
 
   describe '#check_diagonal_dexter' do
